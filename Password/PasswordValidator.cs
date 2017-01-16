ff7940cb49c5fa7901a3929f1445fb72914bdfe6
amupaaru29 <ambikaparvati@gmail.com>
2017-01-16 17:46:23 +0530

    Password Verification - Initial Commit

---
+++ Password/PasswordValidator.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Password
+{
+    public class PasswordValidator
+    {
+        public bool Verify(string password, string usertype)
+        {
+            if (usertype.Equals("Internal"))
+            {
+                InternalPassword _InternalPassword = new InternalPassword();
+                return _InternalPassword.VerifyInternal(password);
+            }
+            else if (usertype.Equals("External"))
+            {
+                ExternalPassword _ExternalPassword = new ExternalPassword();
+                return _ExternalPassword.VerifyExternal(password);
+            }
+            else throw (new Exception("Wrong usertype"));
+        }
+    }
+
+    public class InternalPassword : PasswordValidator
+    {
+        public bool VerifyInternal(string password)
+        {
+            if (password.Length > 8) return true;
+            else throw (new Exception("Password should be larger than 8 chars"));
+        }
+    }
+    public class ExternalPassword : PasswordValidator
+    {
+        public int count = 0;
+        public bool VerifyExternal(string str)
+        {
+            if (CheckLength(str) && CheckNull(str) && CheckUpperCase(str) && CheckLowerCase(str) && CheckNumber(str))
+                return true;
+            else if (count > 2)
+                return true;
+            else if (!CheckUpperCase(str))
+                return false;
+            else return false;
+        }
+        public bool CheckLength(string str)
+        {
+            if (str.Length > 8)
+            {
+                count = count + 1;
+                return true;
+            }
+            else throw (new Exception("Password should be larger than 8 chars"));
+        }
+        public bool CheckNull(string str)
+        {
+            if (str != "")
+            {
+                count = count + 1;
+                return true;
+            }
+            else throw (new Exception("Password should not be null"));
+        }
+        public bool CheckUpperCase(string str)
+        {
+            if (str.Any(char.IsUpper))
+            {
+                count = count + 1;
+                return true;
+            }
+            else throw (new Exception("Password should have one uppercase letter at least"));
+        }
+        public bool CheckLowerCase(string str)
+        {
+            if (str.Any(char.IsLower))
+            {
+                count = count + 1;
+                return true;
+            }
+            else throw (new Exception("Password should have one lowercase letter at least"));
+        }
+        public bool CheckNumber(string str)
+        {
+            if (str.Any(char.IsDigit))
+            {
+                count = count + 1;
+                return true;
+            }
+            else throw (new Exception("Password should have one number at least"));
+        }
+    }
+}
---
+++ PasswordTest1/UnitTest1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Password;
+
+namespace Password
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static PasswordValidator PasswordConstructor()
+        {
+            return new PasswordValidator();
+        }
+        private static ExternalPassword ExternalConstructor()
+        {
+            return new ExternalPassword();
+        }
+        [TestMethod]
+        public void CheckLengthInternal()
+        {
+            bool result = PasswordConstructor().Verify("Ab1defghijk", "Internal");
+            Assert.AreEqual(true, result, "Long enough");
+        }
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void CheckLengthInternalFail()
+        {
+            bool result = PasswordConstructor().Verify("Ab", "Internal");
+            Assert.AreEqual(false, result, "Not long enough");
+        }
+        [TestMethod]
+        public void CheckLengthExternal()
+        {
+            bool result = ExternalConstructor().CheckLength("Ab1defghijk");
+            Assert.AreEqual(true, result, "Long enough");
+        }
+        [TestMethod]
+        public void CheckNullExternal()
+        {
+            bool result = ExternalConstructor().CheckNull("ab");
+            Assert.AreEqual(true, result, "Password not null");
+        }
+        [TestMethod]
+        public void CheckUpperCaseExternal()
+        {
+            bool result = ExternalConstructor().CheckUpperCase("Ab");
+            Assert.AreEqual(true, result, "Contains Upper Case");
+        }
+        [TestMethod]
+        public void CheckLowerCaseExternal()
+        {
+            bool result = ExternalConstructor().CheckLowerCase("hijk");
+            Assert.AreEqual(true, result, "Contains Lower Case");
+        }
+        [TestMethod]
+        public void CheckNumberExternal()
+        {
+            bool result = ExternalConstructor().CheckNumber("Ab1def");
+            Assert.AreEqual(true, result, "Contains Number");
+        }
+        [TestMethod]
+        public void Feature1()
+        {
+            bool result = PasswordConstructor().Verify("Ab1defghijk", "External");
+            Assert.AreEqual(true, result, "Correct Password");
+        }
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void Feature2()
+        {
+            bool result = PasswordConstructor().Verify("Ab1","External");
+            Assert.AreEqual(true, result, "3 out of 5 works");
+        }
+        [TestMethod, ExpectedException(typeof(Exception))]
+        public void Feature2Fail()
+        {
+            bool result = PasswordConstructor().Verify("AAA", "External");
+            Assert.AreEqual(false, result, "3 out of 5 don't work");
+        }
+
+    }
+}
